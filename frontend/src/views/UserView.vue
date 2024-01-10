c2c21f043658c4f04d71a3f263b02b17ce9daa50
imkozin <ivan.kozin@hotmail.com>
2024-01-10 15:44:17 +0200

    made front responsive

--- frontend/src/views/UserView.vue
+++ frontend/src/views/UserView.vue
@@ -1,184 +1,191 @@
 <template>
     <div class="container-fluid">
-        <div v-if="!isAdmin">
-    <h2 class="alert alert-info mt-2">User Personal Information</h2>
-    <div class="container mt-4">
-            <div class="row">
-                <div class="col-md-4">
-                    <label class="font-weight-bold">First Name:</label>
-                    <p>{{ user.fname }}</p>
-                </div>
-                <div class="col-md-4">
-                    <label class="font-weight-bold">Last Name:</label>
-                    <p>{{ user.lname }}</p>
-                </div>
-                <div class="col-md-4">
-                    <label class="font-weight-bold">City:</label>
-                    <p>{{ user.city }}</p>
-                </div>
-            </div>
-
-            <div class="row mt-3">
-                <div class="col-md-4">
-                    <label class="font-weight-bold">Login (Email)</label>
-                    <p>{{ user.login }}</p>
-                </div>
-                <div class="col-md-4">
-                    <label class="font-weight-bold">Password</label>
-                    <p>{{ '••••••' }}</p>
-                </div>
-                <div class="col-md-4">
-                    <label class="font-weight-bold">Device Quantity:</label>
-                    <p>
-                        <button class="btn btn-info mr-2" @click="decreaseDeviceQty" :disabled="user.device_qty === 0">-</button>
-                        {{ user.device_qty }}
-                        <button class="btn btn-primary ml-2" @click="increaseDeviceQty">+</button>
-                    </p>
-
-                </div>
-            </div>
+    <div v-if="!isAdmin">
+      <h2 class="alert alert-info mt-2">User Personal Information</h2>
+      <div class="container mt-4">
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">First Name:</label>
+            <p>{{ user.fname }}</p>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">Last Name:</label>
+            <p>{{ user.lname }}</p>
+          </div>
         </div>
+
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">City:</label>
+            <p>{{ user.city }}</p>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">Login (Email)</label>
+            <p>{{ user.login }}</p>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">Password</label>
+            <p>{{ '••••••' }}</p>
+          </div>
+          <div class="col-md-6 mb-3">
+            <label class="font-weight-bold">Device Quantity:</label>
+            <p>
+              <button class="btn btn-info mr-2" @click="decreaseDeviceQty" :disabled="user.device_qty === 0">-</button>
+              {{ user.device_qty }}
+              <button class="btn btn-primary ml-2" @click="increaseDeviceQty">+</button>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
 
         <div v-else class="row mt-3">
-            <div class="col-md-7">
-                <h2 class="alert alert-success">List of Users</h2>
-                <table class="table table-bordered mt-4">
-                <thead>
-                    <tr>
-                        <th scope="col">First Name</th>
-                        <th scope="col">Last Name</th>
-                        <th scope="col">Login (Email)</th>
-                        <th scope="col">Password</th>
-                        <th scope="col">City</th>
-                        <th scope="col">Device Quantity</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="user in users">
-                    <td>{{user.fname}}</td>
-                    <td>{{user.lname}}</td>
-                    <td>{{user.login}}</td>
-                    <td>{{ '••••••' }}</td>
-                    <td>{{user.city}}</td>
-                    <td >{{ user.device_qty ? user.device_qty : 0}}</td>
-                    <td>
-                        <a href="#" class="edit" title="" ><button @click="editBtn(user.id)" class="btn btn-success btn-sm">Edit</button></a>
-                        <a href="#" class="edit ml-1" title="" ><button @click="deleteUser(user.id)"  class="btn btn-danger btn-sm">Delete</button></a>
-                    </td>
-                    </tr>
-                </tbody>
-                </table>
-            </div>
+    <div class="col-md-7">
+      <h2 class="alert alert-success">List of Users</h2>
+      <div class="table-responsive mt-4">
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <th scope="col">First Name</th>
+              <th scope="col">Last Name</th>
+              <th scope="col">Login (Email)</th>
+              <th scope="col">Password</th>
+              <th scope="col">City</th>
+              <th scope="col">Device Quantity</th>
+              <th scope="col">Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>{{ user.fname }}</td>
+              <td>{{ user.lname }}</td>
+              <td>{{ user.login }}</td>
+              <td>{{ '••••••' }}</td>
+              <td>{{ user.city }}</td>
+              <td>{{ user.device_qty ? user.device_qty : 0 }}</td>
+              <td>
+                <a href="#" class="edit" title="">
+                  <button @click="editBtn(user.id)" class="btn btn-success btn-sm">Edit</button>
+                </a>
+                <a href="#" class="edit ml-1" title="">
+                  <button @click="deleteUser(user.id)" class="btn btn-danger btn-sm">Delete</button>
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
 
             <div class="col-md-5">
                 <div v-if="isEditForm">
                     <h2 class="alert alert-warning">Edit User Details</h2>
                     <form @submit.prevent="editUser(currentUser.id)">
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">First Name</label>
-                            <input type="text" class="form-control" v-model="currentUser.fname">
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Last Name</label>
-                            <input type="text" class="form-control" v-model="currentUser.lname">
-                            </div>
-                        </div>
-                        </div>
-
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">City</label>
-                            <input type="text" class="form-control" v-model="currentUser.city">
-                            </div>
-                        </div>
-                        
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Login (Email)</label>
-                            <input type="email" class="form-control" v-model="currentUser.login">
-                            </div>
-                        </div>
-                        </div>
-
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Password</label>
-                            <input type="password" class="form-control" v-model="currentUser.password">
-                            </div>
-                        </div>
-
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Devices</label>
-                            <input type="text" class="form-control" v-model="currentUser.device_qty">
-                            </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">First Name</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="currentUser.fname">
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-success float-left ml-2">Update</button>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Last Name</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="currentUser.lname">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">City</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="currentUser.city">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Login (Email)</label>
+                        <div class="col-sm-10">
+                        <input type="email" class="form-control" v-model="currentUser.login">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
+                        <input type="password" class="form-control" v-model="currentUser.password">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Devices</label>
+                        <div class="col-sm-10">
+                        <input type="text" class="form-control" v-model="currentUser.device_qty">
+                        </div>
+                    </div>
+
+                    <div class="form-group row">
+                        <div class="col-sm-10 offset-sm-2">
+                        <button type="submit" class="btn btn-success float-left ml-2">Update</button>
+                        </div>
+                    </div>
                     </form>
                 </div>
 
                 <div v-else>
                     <h2 class="alert alert-info">Create A New User</h2>
                     <form @submit.prevent="createUser()">
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">First Name</label>
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">First Name</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" v-model="currentUser.fname">
-                            </div>
+                        </div>
                         </div>
 
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Last Name</label>
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Last Name</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" v-model="currentUser.lname">
-                            </div>
                         </div>
                         </div>
 
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">City</label>
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">City</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" v-model="currentUser.city">
-                            </div>
                         </div>
-                        
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Login (Email)</label>
+                        </div>
+
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Login (Email)</label>
+                        <div class="col-sm-10">
                             <input type="email" class="form-control" v-model="currentUser.login">
-                            </div>
                         </div>
                         </div>
 
-                        <div class="row">
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Password</label>
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Password</label>
+                        <div class="col-sm-10">
                             <input type="password" class="form-control" v-model="currentUser.password">
-                            </div>
+                        </div>
                         </div>
 
-                        <div class="col">
-                            <div class="form-group">
-                            <label class="form-label float-left ml-2">Devices</label>
+                        <div class="form-group row">
+                        <label class="col-sm-2 col-form-label">Devices</label>
+                        <div class="col-sm-10">
                             <input type="text" class="form-control" v-model="currentUser.device_qty">
-                            </div>
                         </div>
                         </div>
-                        <button type="submit" class="btn btn-success float-left ml-2">Save</button>
+
+                        <div class="form-group row">
+                        <div class="col-sm-10 offset-sm-2">
+                            <button type="submit" class="btn btn-success float-left ml-2">Save</button>
+                        </div>
+                        </div>
                     </form>
-                </div>
+                    </div>
             </div>
         </div>
     </div>
